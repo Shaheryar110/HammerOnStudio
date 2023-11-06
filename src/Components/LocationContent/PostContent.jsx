a2612ba36fc06701cecb77e74e57fada271184b7
Mudasir1406 <mudasir1406@gmail.com>
2023-11-06 11:14:27 -0800

    location pages done

--- src/Components/LocationContent/PostContent.jsx
+++ src/Components/LocationContent/PostContent.jsx
@@ -4,8 +4,16 @@ import React from "react";
 function PostContent({ posts, postHeading, postpara }) {
   return (
     <>
-      <Box sx={{ width: "100%", height: "100%", paddingY: "5rem" }}>
-        <Container sx={{ maxWidth: "1200px" }}>
+      <Box sx={{ width: "100%", height: "60vh", paddingY: "5rem" }}>
+        <Container
+          sx={{
+            maxWidth: { lg: "1000px" },
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
           <Typography
             sx={{
               textAlign: "center",
@@ -21,6 +29,7 @@ function PostContent({ posts, postHeading, postpara }) {
               textAlign: "center",
               fontWeight: 500,
               fontSize: "18px",
+              paddingTop: "12px",
             }}
           >
             {postpara}
--- src/utils/locationData.js
+++ src/utils/locationData.js
@@ -1,3 +1,35 @@
+// export const servicesArray = [
+//   {
+//     heading: "Our Roof Installation and Repair Services in Monroe, NY",
+//     paragraph:
+//       "At Hammer-on Studios, we're the go-to choice in Monroe, NY construction, for comprehensive construction and renovation services. Our experienced team excels in various areas, delivering excellence and creativity in every project. When it comes to roofing services in Monroe, NY, we're known for top-quality work, whether it's a home remodeling Monroe, NY project or a full-scale Monroe, NY renovation contractors service. Our Monroe, NY, roofing services, including roof installation and repair, ensure your roof is in good hands if you're looking to transform your house into a dream home. Moreover, our Monroe, NY kitchen and bath remodel services in Monroe, NY, are tailored to your vision. As leading residential and commercial construction experts in Monroe, NY, we take pride in rejuvenating both residential and commercial spaces.",
+//     metaTitle:
+//       "Expert Construction and Renovation in Monroe, NY – Hemmer On Studios",
+//     metaDesc:
+//       "At Hammer-on Studios, we're your trusted partner for all things residential and commercial construction in Monroe, NY.",
+//     slug: "Installation",
+//   },
+//   {
+//     heading: "Specialized Kiryas Joel NY Bathroom Renovations at Your Disposal",
+//     paragraph:
+//       "At Hammer-on Studios, we are your trusted source for superior Kiryas Joel NY bathroom renovations. Our dedicated home improvement specialists are here to transform your space with top-quality bathroom renovations. Whether you have a specific vision or need assistance in crafting the perfect design, our team is ready to make your dream bathroom a reality. We take pride in delivering excellence and creativity in every project. Choose Hammer-on Studios for Kiryas Joel NY bathroom renovations that exceed your expectations.",
+//     metaTitle:
+//       "Kiryas Joel NY Bathroom Renovations Experts | Hemmer-On Studios",
+//     metaDesc:
+//       "Discover exceptional Kiryas Joel NY bathroom renovations by home improvement specialists. Upgrade your space with our top-quality bathroom renovations.",
+//     slug: "Renovations",
+//   },
+//   {
+//     heading: "Your Trusted Cornwall NY Construction Company",
+//     paragraph:
+//       "Welcome to Hammer-on Studios, the foremost choice among Cornwall, NY, construction companies. Our commitment to customer satisfaction is unmatched as we bring your home improvement dreams to life. With a dedicated team of home improvement specialists, we excel in a variety of services, from basement remodeling to Kiryas Joel, NY, bathroom renovations. We believe in delivering top-quality work that not only meets but exceeds your expectations. Choose us for your next project, and experience the difference.",
+//     metaTitle: "Top Cornwall NY Construction Company | Hammer-On Studios",
+//     metaDesc:
+//       "Discover Cornwall, NY, construction excellence with our home improvement specialists. From basement remodeling to bathroom renovations, we prioritize customer satisfaction.",
+//     slug: "thirdObject",
+//   },
+// ];
+
 export const servicesArray = [
   {
     heading: "Our Roof Installation and Repair Services in Monroe, NY",
@@ -26,7 +58,174 @@ export const servicesArray = [
     metaTitle: "Top Cornwall NY Construction Company | Hammer-On Studios",
     metaDesc:
       "Discover Cornwall, NY, construction excellence with our home improvement specialists. From basement remodeling to bathroom renovations, we prioritize customer satisfaction.",
-    slug: "thirdObject",
+    slug: "Construction",
+  },
+  {
+    heading: "Expert Stony Point NY Roofing Services by Hammer-On Studios",
+    paragraph:
+      "Say hello to Hammer-on Studios, your go-to source for top-notch Stony Point, NY, roofing services. With a focus on roofing and home repair, our dedicated team ensures your project is in good hands. From roof installation to repair, we bring excellence and creativity to every job. We're committed to delivering the best, exceeding your expectations every time.",
+    metaTitle: "Stony Point NY Roofing Services | Hammer-On Studios",
+    metaDesc:
+      "Trust our experts for Stony Point, NY, roofing services. From roof installation to repair, we deliver excellence in roofing and home repair, exceeding your expectations.",
+    slug: "Roofing",
+  },
+  {
+    heading: "Your West Haverstraw NY Deck Construction Specialists",
+    paragraph:
+      "Welcome to Hammer-on Studios, your reliable choice for West Haverstraw, NY, deck construction. We excel in deck building, offering cost-effective remodeling solutions. Our dedicated team of reliable contractors is committed to turning your outdoor dreams into reality. Experience top-quality deck construction that exceeds your expectations.",
+    metaTitle: "West Haverstraw NY Deck Construction | Hammer-On Studios",
+    metaDesc:
+      "Discover cost-effective West Haverstraw NY deck construction by reliable contractors. We specialize in deck building, delivering top-quality solutions that bring your outdoor dreams to life.",
+    slug: "Haverstraw",
+  },
+  {
+    heading: "Peekskill, NY Painting Contractors for Stunning Results",
+    paragraph:
+      "Welcome to Hammer-on Studios, your trusted Peekskill NY, painting contractor. We specialize in both interior and exterior painting, offering services that enhance your property's value. Our experienced team is dedicated to delivering stunning results that transform your space. Explore the difference with our top-quality painting services.",
+    metaTitle: "Peekskill, NY Painting Contractors | Hammer-On Studios",
+    metaDesc:
+      "Enhance property value with Peekskill, NY, painting contractors. From interior to exterior painting, our skilled team delivers stunning results that transform your space.",
+    slug: "Painting",
+  },
+  {
+    heading:
+      "Transforming Homes in Middletown, NY – Your Home Improvement Specialists",
+    paragraph:
+      "Discover Hammer-on Studios, your one-stop solution for Middletown, NY, home improvement. From siding installation to flooring services and crown molding installation, our experienced renovators are committed to enhancing your living space. We also specialize in roof installation and repair, ensuring top-quality work every time. Experience the difference with us.",
+    metaTitle: "Middletown NY Home Improvement | Hammer-On Studios",
+    metaDesc:
+      "Enhance your living space with Middletown, NY, home improvement specialists. From siding and flooring to crown molding and roofing, our experienced renovators deliver excellence.",
+    slug: "Improvement",
+  },
+  {
+    heading: "Retaining Walls in Haverstraw, NY – Your Trusted Choice",
+    paragraph:
+      "Welcome to Hammer-on Studios, your trusted source for Haverstraw NY retaining walls. We specialize in retaining wall construction, building more than structures – we build long-term customer relationships. Our commitment to excellence ensures your project stands the test of time. Discover the difference with us.",
+    metaTitle: "Haverstraw NY Retaining Walls | Hammer-On Studios",
+    metaDesc:
+      "Trust our experts for Haverstraw NY retaining walls. We specialize in construction and building long-term customer relationships. Experience excellence that stands the test of time.",
+    slug: "Retaining",
+  },
+  {
+    heading: "Suffern NY Kitchen Renovations – Transforming Your Space",
+    paragraph:
+      "Welcome to Hammer-on Studios, your trusted partner for Suffern NY kitchen renovations. We specialize in kitchen renovations, delivering top-quality results that enhance your living space. As a team dedicated to trusted home renovation, we take pride in every project, ensuring your complete satisfaction. From inspection to progress, we keep you in the loop so no corner is left untouched—partner with us today for great value and exceptional results.",
+    metaTitle: "Suffern NY Kitchen Renovations | Hammer-On Studios",
+    metaDesc:
+      "Transform your living space with our Suffern NY kitchen renovations. Our trusted home renovation team excels in kitchen renovations, ensuring top-quality results every time.",
+    slug: "Kitchen",
+  },
+  {
+    heading: "Newburgh NY Roof Repair – Expert Roofing and Home Repair",
+    paragraph:
+      "Discover Hammer-on Studios, your go-to source for Newburgh, NY, roof, repair, and high-quality construction services. Our expert team specializes in roofing and home repair, ensuring your roof is in good hands. We prioritize excellence and quality in every project because we care, and that's how Hammer-On established itself as one of the reliable Contractors in beautiful Newburgh.",
+    metaTitle: "Newburgh NY Roof Repair | Hammer-On Studios",
+    metaDesc:
+      "Trust our experts for Newburgh, NY, roof, repair, and high-quality construction. From roofing to home repair, we prioritize excellence in every project.",
+    slug: "Repair",
+  },
+  {
+    heading: "Cortlandt NY Renovation Contractors – Superior Performance",
+    paragraph:
+      "At Hammer-On Studios, we understand the importance of your home or commercial space and the role it plays in your life. We know that renovations and remodels are not just about aesthetics; they're about improving functionality, enhancing property value, and creating spaces that truly resonate with your vision. That's why our team is dedicated to delivering superior performance and offering cost-effective remodeling solutions, including complete basement remodeling.",
+    metaTitle: "Cortlandt NY Renovation Contractors | Hammer-On Studios",
+    metaDesc:
+      "Experience superior performance with Cortlandt, NY, renovation contractors. Our cost-effective remodeling solutions, including basement remodeling, bring excellence and quality to your projects.",
+    slug: "Performance",
+  },
+  {
+    heading: "Ringwood NJ Commercial Property Remodeling",
+    paragraph:
+      "Welcome to Hammer-on Studios, your dedicated partner for Ringwood, NJ, commercial property remodeling. We specialize in both residential and commercial construction, ensuring your project exceeds expectations. Our commitment to excellence shines through in every endeavor, transforming your commercial property into a masterpiece.",
+    metaTitle: "Ringwood NJ Commercial Property Remodeling | Hammer-On Studios",
+    metaDesc:
+      "Elevate your commercial property with Ringwood, NJ, commercial property remodeling. Our expertise in residential and commercial construction guarantees results that exceed expectations.",
+    slug: "Commercial",
+  },
+  {
+    heading: "New City, NY Construction Services by Experienced Renovators",
+    paragraph:
+      "Discover Hammer-on Studios, your premier choice for New City, NY, construction services. Our team of experienced renovators and home improvement specialists is dedicated to bringing your vision to life. From concept to completion, we deliver top-quality work that transforms your space. That’s our promise of not just words but work; choose us for your renovation brilliance.",
+    metaTitle: "New City, NY Construction Services | Hammer-On Studios",
+    metaDesc:
+      "Transform your space with New City, NY, construction services by experienced renovators. From concept to completion, our experts deliver top-quality work.",
+    slug: "Renovators",
+  },
+  {
+    heading: "Monsey NY Kitchen and Bath Remodeling Experts",
+    paragraph:
+      "Welcome to Hammer-on Studios, your trusted source for Monsey, NY, kitchen and bath remodeling. Our expertise in kitchen and bathroom renovations ensures your space is transformed into a work of art. We are committed to delivering top-quality work that brings your vision to life.",
+    metaTitle: "Monsey NY Kitchen and Bath Remodeling | Hammer-On Studios",
+    metaDesc:
+      "Experience the art of Monsey NY kitchen and bath remodeling. Our experts bring your vision to life with top-quality kitchen and bathroom renovations.",
+    slug: "Bath",
+  },
+  {
+    heading: "Beacon NY Deck Builders for Your Outdoor Oasis",
+    paragraph:
+      "Welcome to Hammer-on Studios, your trusted team of Beacon NY deck builders. We specialize in deck building, turning your outdoor space into an oasis of relaxation. Our commitment to excellence ensures your project is completed to perfection, enhancing your outdoor living experience.",
+    metaTitle: "Beacon NY Deck Builders | Hammer-On Studios",
+    metaDesc:
+      "Elevate your outdoor space with Beacon NY deck builders. Our commitment to excellence in deck building guarantees an outdoor oasis of relaxation.",
+    slug: "Beacon",
+  },
+  {
+    heading: "Our Roof Installation and Repair Services in West Milford, NJ",
+    paragraph:
+      "At Hammer-on Studios, we're the go-to choice in West Milford, NJ construction, for comprehensive construction and renovation services. Our experienced team excels in various areas, delivering excellence and creativity in every project. When it comes to roofing services in West Milford, NJ, we're known for top-quality work, whether it's a home remodeling West Milford, NJ project or a full-scale West Milford, NJ renovation contractor service. Our West Milford, NJ, roofing services, including roof installation and repair, ensure your roof is in good hands if you're looking to transform your house into a dream home. Moreover, our West Milford, NJ kitchen and bath remodel services in West Milford, NJ, are tailored to your vision. As leading residential and commercial construction experts in West Milford, NJ, we take pride in rejuvenating both residential and commercial spaces.",
+    metaTitle:
+      "Expert Construction and Renovation in West Milford, NJ – Hemmer On Studios",
+    metaDesc:
+      "At Hammer-on Studios, we're your trusted partner for all things residential and commercial construction in West Milford, NJ.",
+    slug: "Roof-Installation",
+  },
+  {
+    heading: "Orange County, NY Renovation Contractors – Superior Performance",
+    paragraph:
+      "At Hammer-On Studios, we understand the importance of your home or commercial space and the role it plays in your life. We know that renovations and remodels are not just about aesthetics; they're about improving functionality, enhancing property value, and creating spaces that truly resonate with your vision. That's why our team is dedicated to delivering superior performance and offering cost-effective remodeling solutions, including complete basement remodeling.",
+    metaTitle: "Orange County, NY Renovation Contractors | Hammer-On Studios",
+    metaDesc:
+      "Experience superior performance with Orange County, NY, renovation contractors. Our cost-effective remodeling solutions, including basement remodeling, bring excellence and quality to your projects.",
+    slug: "orange-county",
+  },
+  {
+    heading:
+      "Rockland County, NY Construction Services by Experienced Renovators",
+    paragraph:
+      "Discover Hammer-on Studios, your premier choice for Rockland County, NY, construction services. Our team of experienced renovators and home improvement specialists is dedicated to bringing your vision to life. From concept to completion, we deliver top-quality work that transforms your space. That’s our promise of not just words but work; choose us for your renovation brilliance.",
+    metaTitle: "Rockland County, NY Construction Services | Hammer-On Studios",
+    metaDesc:
+      "Transform your space with Rockland County, NY, construction services by experienced renovators. From concept to completion, our experts deliver top-quality work.",
+    slug: "Renovators",
+  },
+  {
+    heading: "Ulster County, NY Kitchen Renovations – Transforming Your Space",
+    paragraph:
+      "Welcome to Hammer-on Studios, your trusted partner for Ulster County, NY, kitchen renovations. We specialize in kitchen renovations, delivering top-quality results that enhance your living space. As a team dedicated to trusted home renovation, we take pride in every project, ensuring your complete satisfaction. From inspection to progress, we keep you in the loop so no corner is left untouched—partner with us today for great value and exceptional results. ",
+    metaTitle: "Ulster County, NY Kitchen Renovations | Hammer-On Studios",
+    metaDesc:
+      "Transform your living space with our Ulster County, NY, kitchen renovations. Our trusted home renovation team excels in kitchen renovations, ensuring top-quality results every time.",
+    slug: "Ulster",
+  },
+  {
+    heading:
+      "Specialized Dutchess County, NY Bathroom Renovations at Your Disposal",
+    paragraph:
+      "At Hammer-on Studios, we are your trusted source for superior Dutchess County, NY, bathroom renovations. Our dedicated home improvement specialists are here to transform your space with top-quality bathroom renovations. Whether you have a specific vision or need assistance in crafting the perfect design, our team is ready to make your dream bathroom a reality. We take pride in delivering excellence and creativity in every project. Choose Hammer-on Studios for Dutchess County, NY, bathroom renovations that exceed your expectations.",
+    metaTitle:
+      "Dutchess County, NY Bathroom Renovations Experts | Hemmer-On Studios",
+    metaDesc:
+      "Discover exceptional Dutchess County, NY, bathroom renovations by home improvement specialists. Upgrade your space with our top-quality bathroom renovations.",
+    slug: "Dutchess",
+  },
+  {
+    heading: "Putnam County, NY Roof Repair – Expert Roofing and Home Repair",
+    paragraph:
+      "Discover Hammer-on Studios, your go-to source for Putnam County, NY, roof, repair, and high-quality construction services. Our expert team specializes in roofing and home repair, ensuring your roof is in good hands. We prioritize excellence and quality in every project because we care, and that's how Hammer-On established itself as one of the reliable Contractors in beautiful Newburgh. ",
+    metaTitle: "Putnam County, NY Roof Repair | Hammer-On Studios",
+    metaDesc:
+      "Trust our experts for Putnam County, NY, roof, repair, and high-quality construction. From roofing to home repair, we prioritize excellence in every project.",
+    slug: "Putnam",
   },
 ];
 
