52b9a3f28f727b8aed775d83fcd8a4860981bea7
Mudasir1406 <mudasir1406@gmail.com>
2023-11-29 00:19:52 +0500

    push tag manager

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,28 +18,6 @@ export default function Home() {
   return (
     <>
       <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-6ZHJ2LJG7C"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          {function gtag() {
-            dataLayer.push(arguments);
-          }}
-          gtag('js', new Date()); gtag('config', 'G-6ZHJ2LJG7C');
-        </script>
-        {/* <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-4XF3Y3MXGZ"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          {function gtag() {
-            dataLayer.push(arguments);
-          }}
-          gtag('js', new Date()); gtag('config', 'G-4XF3Y3MXGZ');
-        </script> */}
         <title>
           Office Renovation Contractor | Flat Roof Repair Nyc | Hammer On
           Studios
@@ -95,6 +73,18 @@ export default function Home() {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(BissunessSchema) }}
         ></script>
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6ZHJ2LJG7C"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          {function gtag() {
+            dataLayer.push(arguments);
+          }}
+          gtag('js', new Date()); gtag('config', 'G-6ZHJ2LJG7C');
+        </script>
       </Head>
       <main>
         <Layout>
