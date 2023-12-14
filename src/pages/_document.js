c7aed52eb2d625bae3000440dc114376637171e7
Mudasir1406 <mudasir1406@gmail.com>
2023-12-15 00:53:00 +0500

    toprevenuegate

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,11 +5,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <script
+        <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-6ZHJ2LJG7C"
         ></script>
-       
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -26,6 +26,10 @@ export default function Document() {
           data-account="MDH5wuVUUW"
         ></script>
         <link rel="icon" type="image/x-icon" href="/favicon.webp" />
+        <script
+          type="text/javascript"
+          src="//pl21780114.toprevenuegate.com/d7/87/78/d787785326d86c48232cd590cd299353.js"
+        ></script>
       </Head>
 
       <body>
