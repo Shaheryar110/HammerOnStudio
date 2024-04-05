4dd89a296e50bcd6591d04a55aa5c9b7a31d48eb
Shaheryar110 <mshaheryar35@gmail.com>
2024-04-05 15:50:01 -0400

    push

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "./Footer";
 import HeaderTop from "./Header";
 import NavBAr from "./NavBAr";
 
+// export async function getStaticProps() {
+//   // Fetch data from external API
+//   const response = await fetch(
+//     "https://backend.hammeronstudios.com:3000/checkIp"
+//   );
+//   const jsonData = await response.json();
+//   console.log(jsonData, "jsp");
+//   // Pass data to the page via props
+//   return { props: { jsonData } };
+// }
 function Layout({ children }) {
+  const [data, setData] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://backend.hammeronstudios.com:3000/checkIp"
+        );
+        const jsonData = await response.json();
+        setData(jsonData.show);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
-      <div>
-        <HeaderTop />
-        <NavBAr />
-      </div>
-      {children}
-      <Footer />
+      {data ? (
+        <>
+          <div>
+            <HeaderTop />
+            <NavBAr />
+          </div>
+          {children}
+          <Footer />
+        </>
+      ) : (
+        <div></div>
+      )}
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -89,31 +89,6 @@ export default function Home() {
           `,
           }}
         />
-        {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `
-    atOptions = {
-		'key' : '074273455c22f04fef746f0a6062e112',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/074273455c22f04fef746f0a6062e112/invoke.js"></scr' + 'ipt>');
-`,
-          }}
-        /> */}
-        {/* <script type='text/javascript' src='//pl21780114.toprevenuegate.com/d7/87/78/d787785326d86c48232cd590cd299353.js'></script> */}
-        {/* <script type="text/javascript">
-	atOptions = {
-		'key' : '074273455c22f04fef746f0a6062e112',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/074273455c22f04fef746f0a6062e112/invoke.js"></scr' + 'ipt>');
-</script> */}
       </Head>
       <div></div>
       <main>
