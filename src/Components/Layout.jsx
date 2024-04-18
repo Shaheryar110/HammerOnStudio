6f3b008baf8fe0c21d1614ce2922e4be3611d2fd
Shaheryar110 <mshaheryar35@gmail.com>
2024-04-18 17:18:03 -0400

    page not fonnd

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Footer from "./Footer";
 import HeaderTop from "./Header";
 import NavBAr from "./NavBAr";
+import { Box, Container, Typography } from "@mui/material";
+import ReportIcon from "@mui/icons-material/Report";
 
 // export async function getStaticProps() {
 //   // Fetch data from external API
@@ -15,25 +17,31 @@ import NavBAr from "./NavBAr";
 // }
 function Layout({ children }) {
   const [data, setData] = useState(true);
+  const [loading, SetLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         "https://backend.hammeronstudios.com:3000/checkIp"
-  //       );
-  //       const jsonData = await response.json();
-  //       setData(jsonData.show);
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   };
+  useEffect(() => {
+    const fetchData = async () => {
+      SetLoading(true);
+      try {
+        const response = await fetch(
+          "https://backend.hammeronstudios.com:3000/checkIp"
+        );
+        const jsonData = await response.json();
+        setData(jsonData.show);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        SetLoading(false);
+      }
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
   return (
     <>
-      {data ? (
+      {loading ? (
+        <div></div>
+      ) : data ? (
         <>
           <div>
             <HeaderTop />
@@ -43,7 +51,19 @@ function Layout({ children }) {
           <Footer />
         </>
       ) : (
-        <div></div>
+        <>
+          <Box sx={{ width: "100%", height: "100%", paddingY: "4rem" }}>
+            <Container sx={{ maxWidth: { lg: "1450px" } }}>
+              <ReportIcon sx={{ fontSize: 150 }} />
+              <Typography variant="h2" sx={{ marginY: "1rem" }}>
+                This site can’t be reached
+              </Typography>
+              <Typography>
+                This Site Can't be open execpt USA try from different ip
+              </Typography>
+            </Container>
+          </Box>
+        </>
       )}
     </>
   );
